94c590d2e91822c8044a1802c91a8c63cd1f4487
class <class@CST_N417_05.cst.citytech.cuny.edu>
2015-02-18 17:30:07 -0500

    Finished exercise

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,7 +16,17 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       String name;
+        int age;
+        double annualPay;
+        
+        name = "Aleksandra";
+        age = 21;
+        annualPay = 99000.00;
+                
+        
+        System.out.println("My name is " + name + "," + " I am " + age + " years old" );
+        System.out.println("I hope to earn $"+ annualPay + " per year. ");
     }
     
 }
